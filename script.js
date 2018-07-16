1a0ccca56af223c8484e292b85c737d91ce57711
Travis <Travisbatts6@gmail.com>
2018-07-16 11:16:52 -0400

    first initialize

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+
+// $("p").on("click", function(){
+//   $(this).fadeOut(1000);
+// });
+
+
+// $("p").addClass("colorful timestwo");
+// $("p").removeClass("timestwo");
+
+// $("#first_btn").on('click', function(){
+  //code in here will be executed when user clicks the element //
+//   $("#box").slideToggle(1000);
+// });
+
+// $("#first_btn").on("click", function(){
+//   $(".dope").text("This is new replacement text");
+//   $("#first_btn").val("dbl click me");
+//
+//  });
+// $("#first_btn").on("dblclick", function(){
+//   $("#first_btn").val("you bastard!");
+//   $(".dope").text("now i'm replaced with a bastard")
+//  });
+//
+//  $("#btn_new").on("click", function(){
+//    $(".dope").html('<b class="cool">Hello world</b>')
+//  });
+
+// $("form").on("submit", function(){
+//   let entered_word = $("input#entered_word").val();
+//   alert(entered_word);
+//  });
+
+$("button#loc_button").on('click', function(){
+  var enteredZip = $("input#enter_zip")
+ });
+});
